d800fd22a04b5aa739b335c99dd4eea909218185
Hannigan <lhnng@bcd07466147c.ant.amazon.com>
2022-09-07 19:17:09 +0100

    Initial commit

---
+++ test/table_size_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TableSizeCdk from '../lib/table_size_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/table_size_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TableSizeCdk.TableSizeCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
